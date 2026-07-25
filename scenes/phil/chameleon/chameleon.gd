d77ba9a4afbe6760ed286c21e7f3e1992225177b
Phil <philippbecker1@web.de>
2026-07-25 14:33:19 +0200

    added more stuff

--- scenes/phil/chameleon/chameleon.gd
+++ scenes/phil/chameleon/chameleon.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 
+@export_range(0.0, 3.0, 0.1) var max_diff: float = 1.0
+
 @onready var hue_slider: HSlider = $HueSlider
 @onready var sat_slider: HSlider = $SatSlider
 @onready var value_slider: HSlider = $ValueSlider
@@ -12,6 +14,9 @@ var cham_hue: float = 0.25
 var cham_sat: float = 0.8
 var cham_value: float = 0.5
 
+signal win
+signal lose
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -20,6 +25,8 @@ func _ready() -> void:
 	value_slider.set_value_no_signal(cham_value)
 	update_color()
 	random_color()
+	Countdown.start(10)
+	Countdown.ended.connect(check_color.bind(cham_sprite.modulate, background.modulate))
 
 
 func update_color() -> void:
@@ -30,8 +37,14 @@ func random_color() -> void:
 	background.modulate = Color(randf_range(0.0,1.0), randf_range(0.0,1.0),randf_range(0.0,1.0))
 
 
-func calc_score(cham_color: Color, bg_color: Color) -> int:
-	return 0
+func check_color(cham_color: Color, bg_color: Color) -> void:
+	var diff_color: Color = cham_color - bg_color
+	var diff: float = abs(diff_color.r) + abs(diff_color.g) + abs(diff_color.b)
+	print(diff_color)
+	if diff > max_diff:
+		lose.emit()
+	else:
+		win.emit()
 
 
 
